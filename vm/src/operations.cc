b95e1077efc0606dc818cf16ee0394225f93e95c
Joe Fowler <scrumplesplunge@gmail.com>
2016-04-16 19:23:08 +0000

    Wrote a large part of the design section.
    Additionally made small changes to the background, and added a glossary of
    terms.

--- vm/src/operations.cc
+++ vm/src/operations.cc
@@ -68,12 +68,12 @@ string Operation::toString() const {
 // Instruction pattern.
 const char pattern_string[] =
   //     12          2 1
-  R"(^\s*(([A-Z0-9_]+):)?)"                      // Line label (if any)
+  R"(^\s*(([A-Z0-9_]+):)?)"                    // Line label (if any)
     //    34         45   6
     R"(\s*(([a-z0-9]+)(\s+()"                  // Mnemonic, optional:
-      R"(-?0x[0-9a-fA-F]+|)"                     // Hexadecimal argument
-      R"(-?[1-9][0-9]*|)"                        // Decimal argument
-      R"(-?[0-7]+|)"                             // Octal argument
+      R"(-?0x[0-9a-fA-F]+|)"                   // Hexadecimal argument
+      R"(-?[1-9][0-9]*|)"                      // Decimal argument
+      R"(-?[0-7]+|)"                           // Octal argument
       // 7          78       9          98
       R"(([A-Z0-9_]+)(\s*-\s*([A-Z0-9_]+))?)"  // Symbolic argument
     // 65 3
